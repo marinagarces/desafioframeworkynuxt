3a4eddb65a1310944c7c92c465174d81e5025af4
marinagarces <31573683+marinagarces@users.noreply.github.com>
2021-11-03 21:39:03 -0300

    first commit

---
+++ components/Personajes.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <v-card class="mx-auto" max-width="100%">
+      <v-img class="white--text align-end" :src="personaje.imagen">
+        <v-card-title> {{ personaje.nombre }} </v-card-title>
+      </v-img>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    personaje: Object,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ pages/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <v-row justify="center" class="mt-12">
+      <v-col cols="11">
+        <v-row>
+          <v-col v-for="personaje in personajes" :key="personaje.id" lg="4" xl="4" md="4" sm="3" cols="12">
+            <Personajes :personaje="personaje" />
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import Personajes from '../components/Personajes'
+
+export default {
+  name: 'Home',
+  components: {
+    Personajes,
+  },
+  computed: {
+    personajes() {
+      return this.$store.getters.getPersonajes
+    },
+  },
+}
+</script>
